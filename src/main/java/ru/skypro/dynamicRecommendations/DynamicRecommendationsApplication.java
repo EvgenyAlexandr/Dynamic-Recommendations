4ba3642b3c1f54bfd688a092fa8c3a313a52a53c
EvgenyAlexand <bragin-e@mail.ru>
2026-04-23 07:26:03 +0700

    Init commit

---
+++ src/main/java/ru/skypro/dynamicRecommendations/DynamicRecommendationsApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.dynamicRecommendations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicRecommendationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicRecommendationsApplication.class, args);
+	}
+
+}
